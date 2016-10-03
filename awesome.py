f7f78bc84becf5b4ba2331ab7d0fdc2621d11aad
citiusgittest <servandoplatform@gmail.com>
2016-10-03 18:31:28 +0200

    Fixed error

--- awesome.py
+++ awesome.py
@@ -4,7 +4,7 @@ num = int(raw_input("Please enter an integer: "))
 
 if num < 0:
       num = 0
-      print 'Negtive changed to zero'
+      print 'Negative changed to zero'
 elif num == 0:
       print 'Zero'
 elif num == 1:
