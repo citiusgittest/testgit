7ff43ee895b3109641a3f91d01554acad6ae2105
angelpinheiro <angelpinheirosouto@gmail.com>
2016-10-03 17:38:52 +0200

    Engadida funcionalidade

---
+++ awesome.py
@@ -0,0 +1,14 @@
+#! python
+
+num = int(raw_input("Please enter an integer: "))
+
+if num < 0:
+      num = 0
+      print 'Negtive changed to zero'
+elif num == 0:
+      print 'Zero'
+elif num == 1:
+      print 'One'
+else:
+      print 'More'
+
